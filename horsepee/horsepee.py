8ddb7bbf662a777a7274f4e1aadad86a621f95c1
Alexander Elzenaar <albeetle@gmail.com>
2018-10-02 16:09:00 -0400

    Preprocessor works now if you pipe it into the compiler directly.

--- horsepee/horsepee.py
+++ horsepee/horsepee.py
@@ -1,2 +1,33 @@
+import json
 import pystache
 
+def decode(filename):
+  with open(filename) as f:
+    parsed = {}
+    inContent = False
+    decoder = json.JSONDecoder()
+    content = ""
+    for line in f:
+      if("===" in line):
+        #print(parsed)
+        #print("end keys, content now")
+        inContent = True
+        continue
+      if(not(inContent)):
+        key = decoder.decode(line)
+        #print(key)
+        parsed.update(key)
+      else:
+        #print(line)
+        content = content + line
+
+    parsed["content"] = content
+    return parsed
+
+def fill(template, data):
+  with open(template) as f:
+    templateContent = f.read()
+    return pystache.render(templateContent, data)
+
+
+print(fill("template.mustache", decode("test.tex")))
