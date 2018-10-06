31687ad6182d9114aca361e61574f70e9f587679
Alexander Elzenaar <albeetle@gmail.com>
2018-10-06 12:27:29 -0400

    File serving works

--- web/server.py
+++ web/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, redirect, url_for, abort, render_template
+from flask import Flask, redirect, url_for, abort, render_template, make_response
 app = Flask(__name__)
 
 from pathlib import Path
 from tinydb import TinyDB, Query, where
 
 import calendar
+import magic
 
 datadir = Path("../db/data")
 dbname = "database.db"
@@ -31,7 +32,7 @@ def v_time(year=None,month=None,day=None,meat=None):
       if not(item['ts_year'] in years):
         years.append({'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']})
     years.sort()
-    return render_template('viewer.html',items=years,view_name='year')
+    return render_template('viewer.html',items=years,view_name='year', breadcrumbs=[{'loc':'/v/time/','name':'By date',  'current':1}])
 
   if(month == None):
     months = []
@@ -39,7 +40,9 @@ def v_time(year=None,month=None,day=None,meat=None):
       if not(item['ts_month'] in months):
         months.append({'loc':'/v/time/'+str(item['ts_year'])+str('/')+str(item['ts_month']),'name':calendar.month_name[item['ts_month']]})
     months.sort()
-    return render_template('viewer.html',items=months,view_name='month', breadcrumbs=[{'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year'], 'current':1}])
+    return render_template('viewer.html',items=months,view_name='month',
+                           breadcrumbs=[{'loc':'/v/time/','name':'By date'},
+                                        {'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year'], 'current':1}])
 
   if(day == None):
     days = []
@@ -49,7 +52,8 @@ def v_time(year=None,month=None,day=None,meat=None):
                        'name':item['ts_day']})
     days.sort()
     return render_template('viewer.html',items=days,view_name='day',
-                           breadcrumbs=[{'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']},
+                           breadcrumbs=[{'loc':'/v/time/','name':'By date'},
+                                        {'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']},
                                         {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']), 'name': calendar.month_name[item['ts_month']], 'current':1}])
 
   if(meat == None):
@@ -58,7 +62,8 @@ def v_time(year=None,month=None,day=None,meat=None):
       docs.append({'loc':'/v/time/'+str(item['ts_year'])+str('/')+str(item['ts_month']) + str('/') + str(item['ts_day']) + str('/') + item['Buc_name'],
                    'name':item['Buc_name'] + " (at " + str(item["ts_hour"]) + ":" + str(item['ts_minute']) + ":" + str(item['ts_second']) + ") [tags: " + str(item['Buc_tags'].split(',')) + "]"})
     return render_template('viewer.html',items=docs,view_name='article',
-                           breadcrumbs=[{'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']},
+                           breadcrumbs=[{'loc':'/v/time/','name':'By date'},
+                                        {'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']},
                                         {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']), 'name': calendar.month_name[item['ts_month']]},
                                         {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']) + '/' + str(item['ts_day']),
                                          'name':item['ts_day'], 'current':1}])
@@ -77,4 +82,38 @@ def v_time(year=None,month=None,day=None,meat=None):
   source = item['Buc_source'] if 'Buc_source' in item else None
 
   return render_template('article.html', article_name=item['Buc_name'], article_timestamp=timestamp, article_title=item['Buc_title'],
-                                         article_raw=rawname, article_src=source)
+                         article_raw=rawname, article_src=source,
+                         breadcrumbs=[{'loc':'/v/time/','name':'By date'},
+                                      {'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']},
+                                      {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']), 'name': calendar.month_name[item['ts_month']]},
+                                      {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']) + '/' + str(item['ts_day']), 'name':item['ts_day']},
+                                      {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']) + '/' + str(item['ts_day']) + '/' + meat, 'name':meat, 'current':1}])
+
+@app.route('/r/')
+@app.route('/r/<ident>/')
+@app.route('/r/<ident>/<meat>')
+def r_file(ident=None,meat=None):
+  if (ident == None) | (meat == None):
+    return redirect(url_for('index'))
+
+  db = TinyDB(datadir/dbname)
+  item = db.table('files').get(doc_id=int(ident))
+  print(item)
+  if item == None:
+    return abort(404)
+  if not(item['Buc_name'] == meat):
+    return abort(404)
+
+  filename = datadir/str(item['ts_year'])/str(item['ts_month'])/str(item['ts_day'])/str(item['Buc_name'])
+  with open(filename) as f:
+    data = f.read()
+
+  resp = make_response(data)
+  # Fudge the mimetypes
+  mime = magic.Magic(mime=True).from_file(str(filename))
+  if ("text/" in mime) & (not("html" in mime)):
+    resp.headers['Content-Type'] = 'text/plain'
+  else:
+    resp.headers['Content-Type'] = mime
+  print(resp)
+  return resp
