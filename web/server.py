aa31f22d6b59962f4eb50373862d1bf232e0fc23
Alexander Elzenaar <albeetle@gmail.com>
2018-10-06 03:30:12 -0400

    Web server basically works now with dates and things

---
+++ web/server.py
@@ -0,0 +1,67 @@
+from flask import Flask, redirect, url_for, abort, render_template
+app = Flask(__name__)
+
+from pathlib import Path
+from tinydb import TinyDB, Query, where
+
+import calendar
+
+datadir = Path("../db/data")
+dbname = "database.db"
+
+@app.route('/')
+def index():
+    with open('index.html') as f:
+      return f.read()
+
+@app.route('/v/')
+def noview():
+  return redirect(url_for('index'))
+
+@app.route('/v/time/')
+@app.route('/v/time/<year>/')
+@app.route('/v/time/<year>/<month>/')
+@app.route('/v/time/<year>/<month>/<day>/')
+@app.route('/v/time/<year>/<month>/<day>/<meat>/')
+def v_time(year=None,month=None,day=None,meat=None):
+  db = TinyDB(datadir/dbname)
+
+  if(year == None):
+    years = []
+    for item in db.table('files'):
+      if not(item['ts_year'] in years):
+        years.append({'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']})
+    years.sort()
+    return render_template('viewer.html',items=years,view_name='year')
+
+  if(month == None):
+    months = []
+    for item in db.table('files').search(where('ts_year')==int(year)):
+      if not(item['ts_month'] in months):
+        months.append({'loc':'/v/time/'+str(item['ts_year'])+str('/')+str(item['ts_month']),'name':calendar.month_name[item['ts_month']]})
+    months.sort()
+    return render_template('viewer.html',items=months,view_name='month', breadcrumbs=[{'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year'], 'current':1}])
+
+  if(day == None):
+    days = []
+    for item in db.table('files').search((where('ts_year')==int(year)) & (where('ts_month')==int(month))):
+      if not(item['ts_day'] in days):
+        days.append({'loc':'/v/time/'+str(item['ts_year'])+str('/')+str(item['ts_month']) + str('/') + str(item['ts_day']),
+                       'name':item['ts_day']})
+    days.sort()
+    return render_template('viewer.html',items=days,view_name='day',
+                           breadcrumbs=[{'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']},
+                                        {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']), 'name': calendar.month_name[item['ts_month']], 'current':1}])
+
+  if(meat == None):
+    docs = []
+    for item in db.table('files').search((where('ts_year')==int(year)) & (where('ts_month')==int(month)) & (where('ts_day')==int(day))):
+      docs.append({'loc':'/v/time/'+str(item['ts_year'])+str('/')+str(item['ts_month']) + str('/') + str(item['ts_day']) + str('/') + item['Buc_name'],
+                   'name':item['Buc_name'] + " (at " + str(item["ts_hour"]) + ":" + str(item['ts_minute']) + ":" + str(item['ts_second']) + ") [tags: " + str(item['Buc_tags'].split(',')) + "]"})
+    return render_template('viewer.html',items=docs,view_name='article',
+                           breadcrumbs=[{'loc':'/v/time/'+str(item['ts_year']),'name':item['ts_year']},
+                                        {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']), 'name': calendar.month_name[item['ts_month']]},
+                                        {'loc':'/v/time/'+str(item['ts_year'])+'/'+str(item['ts_month']) + '/' + str(item['ts_day']),
+                                         'name':item['ts_day'], 'current':1}])
+
+  return abort(404)
