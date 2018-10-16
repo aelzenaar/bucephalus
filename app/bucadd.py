40e8a634a00dfaa08b9ca5a729b2ad7444ab9f90
Alexander Elzenaar <albeetle@gmail.com>
2018-10-16 17:51:19 -0400

    Bucadd have nice args

--- app/bucadd.py
+++ app/bucadd.py
@@ -1,20 +1,45 @@
 import sys
+import argparse
+import json
 
 import dbops
+import config
+
 from pathlib import Path
 
-if len(sys.argv) < 4:
-  print("Bucephalus Add File Script")
-  print("Usage: " + sys.argv[0] + " <filename> <title> <author> <tag1> ... <tagN>")
-  sys.exit()
 
-sys.argv.pop(0)
-filename = sys.argv.pop(0)
-title = sys.argv.pop(0)
-author = sys.argv.pop(0)
-tags = sys.argv
+parser = argparse.ArgumentParser(description='Bucephalus: Add File')
+parser.add_argument('filename', metavar='FILENAME', type=str, nargs=1,
+                   help='filename for processing')
+parser.add_argument('title', metavar='TITLE', type=str, nargs=1,
+                   help='title to add to database', default=None)
+parser.add_argument('-a', metavar='AUTHOR', type=str, nargs=1,
+                   help='author name', default=None)
+parser.add_argument('tags', metavar='TAGS', type=str, nargs='+',
+                   help='tags to add to database', default=None)
+
+args = vars(parser.parse_args())
+
+author = args['a'][0] if args['a'] != None else ""
+tags = []
+
+defaults=config.get_user_data_dir()/"defaults.json"
+if defaults.exists():
+  with open(defaults) as f:
+    decoder = json.JSONDecoder()
+    metadata = decoder.decode(f.read())
+    if ('Buc_author' in metadata) and (args['a'] == None):
+      author = metadata['Buc_author']
+    if 'Buc_tags' in metadata:
+      tags = metadata['Buc_tags']
+
+filename = args['filename'][0]
+title = args['title'][0]
+for tag in args['tags']:
+  if not(tag in tags):
+    tags.append(tag)
 
 print(filename)
 
-if dbops.addrecord(title, author, tags, filename) == None:
+if dbops.add_record(title, author, tags, filename) == None:
   print("Error")
