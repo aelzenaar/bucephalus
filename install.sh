3cd301c45b8b59411eb5718b722c08eead46050d
Alex Elzenaar <albeetle@gmail.com>
2018-11-01 22:43:23 -0400

    Clean up source directory & installer

--- install.sh
+++ install.sh
@@ -4,29 +4,25 @@ INSTALLDIR=/opt/bucephalus
 
 rm -r $INSTALLDIR
 mkdir $INSTALLDIR
-cp app/dbops.py $INSTALLDIR
-cp app/bucadd.py $INSTALLDIR
-cp app/server.py $INSTALLDIR
-cp app/bucrm.py $INSTALLDIR
-cp app/config.py $INSTALLDIR
-cp app/search.py $INSTALLDIR
-cp app/bucvac.py $INSTALLDIR
-cp app/bucfup.py $INSTALLDIR
-cp app/bucmup.py $INSTALLDIR
-cp app/tasklist.py $INSTALLDIR
-cp -R app/static $INSTALLDIR
-cp -R app/templates $INSTALLDIR
-cp -R app/prototypes $INSTALLDIR
+cp -R bin $INSTALLDIR
+cp -R web $INSTALLDIR
+cp -R lib $INSTALLDIR
+cp -R prototypes $INSTALLDIR
 
-printf "#!/bin/bash\n FLASK_APP=\"$INSTALLDIR/server.py\" flask run \"\$@\"\n" > /usr/local/bin/bucserve
-printf "#!/bin/bash\n exec python $INSTALLDIR/bucadd.py \"\$@\" \n" > /usr/local/bin/bucadd
-printf "#!/bin/bash\n exec python $INSTALLDIR/bucvac.py \"\$@\" \n" > /usr/local/bin/bucvac
-printf "#!/bin/bash\n exec python $INSTALLDIR/bucfup.py \"\$@\" \n" > /usr/local/bin/bucfup
-printf "#!/bin/bash\n exec python $INSTALLDIR/bucmup.py \"\$@\" \n" > /usr/local/bin/bucmup
-printf "#!/bin/bash\n exec python $INSTALLDIR/bucrm.py \"\$@\" \n" > /usr/local/bin/bucrm
+printf "#!/bin/bash\n FLASK_APP=\"$INSTALLDIR/web/server.py\" PYTHONPATH=\"$PYTHONPATH:$INSTALLDIR/lib\" flask run \"\$@\"\n" > /usr/local/bin/bucserve
 chmod a+rx /usr/local/bin/bucserve
-chmod a+rx /usr/local/bin/bucadd
-chmod a+rx /usr/local/bin/bucfup
-chmod a+rx /usr/local/bin/bucmup
-chmod a+rx /usr/local/bin/bucvac
-chmod a+rx /usr/local/bin/bucrm
+
+function wrapperscript {
+  PLACE="/usr/local/bin/$1"
+  echo "#!/bin/bash" > $PLACE
+  echo "export PYTHONPATH=\"$PYTHONPATH:$INSTALLDIR/lib\"" >> $PLACE
+  echo "exec python $INSTALLDIR/bin/$1.py \"\$@\"" >> $PLACE
+  chmod a+rx $PLACE
+}
+
+wrapperscript bucadd
+wrapperscript bucvac
+wrapperscript bucfup
+wrapperscript bucmup
+wrapperscript bucrm
+
